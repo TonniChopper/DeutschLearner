ca0bc6b9893c2cf5e5cf9a39df80649880c71d89
Andrii <programer1313@gmail.com>
2025-11-19 19:44:32 +0100

    core

--- d_learner_front/src/api/api.jsx
+++ d_learner_front/src/api/api.jsx
@@ -14,33 +14,4 @@ Api.interceptors.request.use(config => {
     return config;
 }, error => Promise.reject(error));
 
-
-// // Example Api service for registration
-// export const registerUser = async (userData) => {
-//     const response = await Api.post('/learning/auth/register/', userData);
-//     localStorage.setItem(ACCESS_TOKEN, response.data.access);
-//     localStorage.setItem(REFRESH_TOKEN, response.data.refresh);
-//     return response.data;
-// };
-//
-// // Similarly, the login function without a try/catch
-// export const loginUser = async (credentials) => {
-//     const response = await Api.post('/learning/auth/login/', credentials);
-//     localStorage.setItem(ACCESS_TOKEN, response.data.access);
-//     localStorage.setItem(REFRESH_TOKEN, response.data.refresh);
-//     return response.data;
-// };
-//
-// // Api service for assignment (corrected endpoint)
-// export const getLessons = async () => {
-//     const response = await Api.get('/learning/assignment/');
-//     return response.data;
-// };
-//
-// // Api service for profile
-// export const getProfile = async () => {
-//     const response = await Api.get('/learning/profile/');
-//     return response.data;
-// };
-
 export default Api;
--- d_learner_front/vite.config.js
+++ d_learner_front/vite.config.js
@@ -4,15 +4,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [react(), tailwindcss()],
-    // server: {
-    //     proxy: {
-    //         '/': {
-    //             target: 'https://serene-woodland-45485-b6d1865e6702.herokuapp.com/',
-    //             changeOrigin: true,
-    //             secure: false
-    //         }
-    //     }
-    // },
     optimizeDeps: {
         esbuildOptions: {
             loader: {
